d7cb599a58c2d09a058e45235d16f8f31c290330
nicolocamassa <camassanicolo03@gmail.com>
2024-01-24 18:23:22 +0100

    [FIX] array fixed single post

--- snack_3/index.php
+++ snack_3/index.php
@@ -1,19 +1,35 @@
 <?php 
     $posts = [
         '27/09/2035' => [
-            'post_id' => 'post 1',
-            'username' => 'Gianni',
-            'description' => 'Post description 1'
+            [
+                'post_id' => 'post 1',
+                'username' => 'Gianni',
+                'description' => 'Post description 1'
+            ],
+            [
+                'post_id' => 'post 1.2',
+                'username' => 'Gianni',
+                'description' => 'Post description 2'
+            ],
         ],
         '12/09/2042' => [
-            'post_id' => 'post 2',
-            'username' => 'Piero',
-            'description' => 'Post description 2'
+            [
+                'post_id' => 'post 2',
+                'username' => 'Piero',
+                'description' => 'Post description 2'
+            ],
         ],
         '13/08/2073' => [
-            'post_id' => 'post 3',
-            'username' => 'Paolo',
-            'description' => 'Post description 3'
+            [
+                'post_id' => 'post 3',
+                'username' => 'Paolo',
+                'description' => 'Post description 3'
+            ],
+            [
+                'post_id' => 'post 4',
+                'username' => 'Mario',
+                'description' => 'Post description 4'
+            ],
         ],
     ]
 ?>
@@ -26,15 +42,15 @@
     <title>SNACK 3</title>
 </head>
 <body>
-    <?php foreach($posts as $date => $post){ ?>
+    <?php foreach($posts as $date => $postList){ ?>
         <div style="margin-top: 20px;"><?php echo $date; ?></div> 
-        <?php foreach($post as $key => $value) { ?>
-            <div><?php echo $value; ?></div>
+        <?php foreach($postList as $post) { ?>
+            <div>
+                <div><?php echo $post['post_id']; ?></div>
+                <div><?php echo $post['username']; ?></div>
+                <div><?php echo $post['description']; ?></div>
+            </div>
         <?php } ?>
-        
     <?php } ?>
-    <!-- TODO: -->
-    <!-- Prendere la key con la data da post  -->
-    <!-- Per ogni chiave prendere le informazioni dei post -->
 </body>
 </html>
