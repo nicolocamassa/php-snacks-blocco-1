04e8ee30fece3781c9e2fdc5832b1e2e2eefd79f
nicolocamassa <camassanicolo03@gmail.com>
2024-01-24 16:06:21 +0100

    [ADD] Added standard layout in index.php

---
+++ snack_1/index.php
@@ -0,0 +1,18 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SNACK 1</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
