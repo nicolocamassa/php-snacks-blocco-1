1510e8505f1ad2f84683f5cbda9b3e67d4f5044c
nicolocamassa <camassanicolo03@gmail.com>
2024-01-24 16:29:21 +0100

    [UPDATE] print of what is taken from the input

--- snack_2/index.php
+++ snack_2/index.php
@@ -1,4 +1,10 @@
 <?php 
+    if(isset($_GET['name'], $_GET['email'], $_GET['age'])){
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $age = $_GET['age'];
+    }
+
 
 ?>
 
@@ -10,6 +16,18 @@
     <title>SNACK 2</title>
 </head>
 <body>
-    
+    <!-- Prendere i dati in input -->
+    <form action="index.php" method="GET">
+        <input type="text" name="name" id="name">
+        <input type="email" name="email" id="email">
+        <input type="number" name="age" id="age">
+        <input type="submit" value="ACCEDI">
+
+        <?php if(isset($name, $email, $age)){?>
+            <div><?php echo $name ?></div>
+            <div><?php echo $email ?></div>
+            <div><?php echo $age ?></div>
+        <?php } ?>
+    </form>
 </body>
 </html>
